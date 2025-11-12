02c61f0ea9c1b36a7f3886fb0f82603491115eb1
Nishant Doma Sawaimoon <nishantcoclover@gmail.com>
2025-11-12 23:53:25 +0530

    Update next.config.ts

--- restaurant-app/next.config.ts
+++ restaurant-app/next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // You can add other Next.js configurations here if needed
 };
 
-export default nextConfig;
+module.exports = nextConfig;
